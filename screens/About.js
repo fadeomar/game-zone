ffe54c4baa6cc75576c37e3eff08396ca581e187
fadeomar <fadeomar2015@gmail.com>
2020-02-17 00:31:19 +0200

    create about screen

---
+++ screens/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const About = () => (
+  <View style={styles.container}>
+    <Text></Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 40
+  }
+});
+
+export default About;
