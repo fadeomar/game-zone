c5e684c4ca314ad95dca88fd32845a20e0a3c313
fadeomar <fadeomar2015@gmail.com>
2020-02-17 00:50:54 +0200

    create review Details screen

---
+++ screens/reviewDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { globleStyle } from "../styles/global";
+
+const ReviewDetails = () => (
+  <View style={globleStyle.container}>
+    <Text style={globleStyle.titleText}>review Details</Text>
+  </View>
+);
+
+export default ReviewDetails;
