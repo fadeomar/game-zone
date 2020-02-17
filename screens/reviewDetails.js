da56911a262e3d0e9160fca012d6e98f476f00b7
fadeomar <fadeomar2015@gmail.com>
2020-02-17 18:35:12 +0200

    using get patam to extract the data

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { globleStyle } from "../styles/global";
 
-const ReviewDetails = () => (
-  <View style={globleStyle.container}>
-    <Text style={globleStyle.titleText}>review Details</Text>
-  </View>
-);
-
-export default ReviewDetails;
+export default function ReviewDetails({ navigation }) {
+  return (
+    <View style={globleStyle.container}>
+      <Text>{navigation.getParam("title")}</Text>
+      <Text>{navigation.getParam("body")}</Text>
+      <Text>{navigation.getParam("rating")}</Text>
+    </View>
+  );
+}
