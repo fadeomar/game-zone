b0e0edf1bcd8d23e7943dfda731d888890544b51
fadeomar <fadeomar2015@gmail.com>
2020-02-17 18:34:36 +0200

    add data to Home components and use hooks

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList
+} from "react-native";
 import { globleStyle } from "../styles/global";
 
-const Home = () => (
-  <View style={globleStyle.container}>
-    <Text style={globleStyle.titleText}>Home</Text>
-  </View>
-);
+export default function Home({ navigation }) {
+  const [reviews, setReviews] = useState([
+    {
+      title: "Zelda, Breath of Fresh Air",
+      rating: 5,
+      body: "lorem ipsum",
+      key: "1"
+    },
+    {
+      title: "Gotta Catch Them All (again)",
+      rating: 4,
+      body: "lorem ipsum",
+      key: "2"
+    },
+    {
+      title: 'Not So "Final" Fantasy',
+      rating: 3,
+      body: "lorem ipsum",
+      key: "3"
+    }
+  ]);
 
-export default Home;
+  return (
+    <View style={globleStyle.container}>
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Text style={globleStyle.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
